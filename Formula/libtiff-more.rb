a5e082ca84b9c3c6af1d11a872ef82257bf3085c
AdiosF6F <62105+Adios@users.noreply.github.com>
2021-02-28 02:46:44 +0800

    libtiff-more: enable WebP & ZSTD compression

--- Formula/libtiff-more.rb
+++ Formula/libtiff-more.rb
@@ -1,5 +1,5 @@
-class Libtiff < Formula
-  desc "TIFF library and utilities"
+class LibtiffMore < Formula
+  desc "TIFF library and utilities (WebP & ZSTD compression)"
   homepage "https://libtiff.gitlab.io/libtiff/"
   url "https://download.osgeo.org/libtiff/tiff-4.2.0.tar.gz"
   mirror "https://fossies.org/linux/misc/tiff-4.2.0.tar.gz"
@@ -11,15 +11,9 @@ class Libtiff < Formula
     regex(/href=.*?tiff[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  bottle do
-    sha256 cellar: :any, arm64_big_sur: "50c09c0a4328c2625147ea78376523e11510ef26e4286953465e922e12d7e045"
-    sha256 cellar: :any, big_sur:       "31dc53e161b68394309dfe7743f9b7f37995b441d66b1637b9424696973b3d40"
-    sha256 cellar: :any, catalina:      "208176ae2c5642eb0dff7c97d7b011d6b1d6317be4242db1e54bbb92609f758b"
-    sha256 cellar: :any, mojave:        "706a61c81bd7a17f266f315339ff63e9bc965c962cd6d9a6a03167cd620d07a1"
-    sha256 cellar: :any, x86_64_linux:  "5c1a4cb09b036d5f67d2afef0070e7ae8e6d89d347f9c86836efd3b49e967a0b"
-  end
-
   depends_on "jpeg"
+  depends_on "webp"
+  depends_on "zstd"
 
   uses_from_macos "zlib"
 
@@ -28,10 +22,12 @@ class Libtiff < Formula
       --prefix=#{prefix}
       --disable-dependency-tracking
       --disable-lzma
-      --disable-webp
-      --disable-zstd
       --with-jpeg-include-dir=#{Formula["jpeg"].opt_include}
       --with-jpeg-lib-dir=#{Formula["jpeg"].opt_lib}
+      --with-webp-include-dir=#{Formula["webp"].opt_include}
+      --with-webp-lib-dir=#{Formula["webp"].opt_lib}
+      --with-zstd-include-dir=#{Formula["zstd"].opt_include}
+      --with-zstd-lib-dir=#{Formula["zstd"].opt_lib}
       --without-x
     ]
     system "./configure", *args
