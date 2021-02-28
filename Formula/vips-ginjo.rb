a7eb1937ce4a9568daefa88447803aaf52ccfe84
AdiosF6F <62105+Adios@users.noreply.github.com>
2021-03-01 04:53:01 +0800

    vips-ginjo: use lossless webp tiff library

--- Formula/vips-ginjo.rb
+++ Formula/vips-ginjo.rb
@@ -1,4 +1,4 @@
-class Vips < Formula
+class VipsGinjo < Formula
   desc "Image processing library"
   homepage "https://github.com/libvips/libvips"
   url "https://github.com/libvips/libvips/releases/download/v8.10.5/vips-8.10.5.tar.gz"
@@ -11,44 +11,24 @@ class Vips < Formula
     strategy :github_latest
   end
 
-  bottle do
-    sha256 arm64_big_sur: "7465e3684f04ad85463694971862aa3694ec67655b01e8d5d6eef6af22852af9"
-    sha256 big_sur:       "423bdd4aa945fefc6dd01646892c429494620cc942a96f25e2eab72cc8088df0"
-    sha256 catalina:      "8d60363727bb9f48e8733b8d570732734ae727809489547e8ec3c956a72569f2"
-    sha256 mojave:        "49429e971e5550fd703dcdcd3647a0d14c08267fa7e5c2ac4f6e0a402e92fb2f"
-  end
-
   depends_on "pkg-config" => :build
-  depends_on "cfitsio"
+  depends_on "expat"
   depends_on "fftw"
   depends_on "fontconfig"
   depends_on "gettext"
-  depends_on "giflib"
   depends_on "glib"
-  depends_on "imagemagick"
   depends_on "libexif"
-  depends_on "libgsf"
   depends_on "libheif"
-  depends_on "libimagequant"
-  depends_on "libmatio"
   depends_on "libpng"
-  depends_on "librsvg"
   depends_on "libspng"
-  depends_on "libtiff"
+  depends_on "adios/ginjo/libtiff-more"
   depends_on "little-cms2"
   depends_on "mozjpeg"
   depends_on "openexr"
-  depends_on "openslide"
   depends_on "orc"
-  depends_on "pango"
-  depends_on "poppler"
   depends_on "webp"
 
   uses_from_macos "zlib"
-
-  on_linux do
-    depends_on "gobject-introspection"
-  end
 
   def install
     # mozjpeg needs to appear before libjpeg, otherwise it's not used
@@ -56,8 +36,20 @@ class Vips < Formula
 
     args = %W[
       --disable-dependency-tracking
+      --disable-introspection
       --prefix=#{prefix}
-      --with-magick
+      --without-cfitsio
+      --without-giflib
+      --without-gsf
+      --without-imagequant
+      --without-magick
+      --without-matio
+      --without-nifti
+      --without-openslide
+      --without-pangoft2
+      --without-pdfium
+      --without-poppler
+      --without-rsvg
     ]
 
     system "./configure", *args
